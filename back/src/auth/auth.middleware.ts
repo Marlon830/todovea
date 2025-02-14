52527e96d6ed32522117564aab027d4137e05b96
Marlon830 <marlonborna2020@gmail.com>
2025-02-14 10:38:16 +0100

    feat(todovea-back): change middleware to use bearer token
    change middleware to use bearer token instead of cookies

--- back/src/auth/auth.middleware.ts
+++ back/src/auth/auth.middleware.ts
@@ -8,13 +8,17 @@ export class AuthMiddleware implements NestMiddleware {
   constructor(private readonly authService: AuthService) { }
 
   use(req: Request, res: Response, next: NextFunction) {
-    const cookieName = process.env.COOKIE_NAME ?? 'auth_token';
+    const authHeader = req.headers.authorization;
 
-    if (req.cookies &&
-      req.cookies[cookieName] &&
-      this.authService.validateUser(req.cookies[cookieName])) {
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(401).json({ message: 'Unauthorized: No token provided' });
+    }
+
+    const token = authHeader.split(' ')[1];
+    if (this.authService.validateUser(token)) {
       return next();
     }
-    return res.status(401).send({ message: 'Unauthorized' });
+
+    return res.status(401).json({ message: 'Unauthorized: Token invalid or expired' });
   }
 }
